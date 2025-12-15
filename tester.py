57568dc10724c47eb307a2241d069e60bd319271
Sqweeezey <124536595+squizi1234@users.noreply.github.com>
2025-12-16 01:57:29 +0400

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,20 @@
+﻿from openai import OpenAI
+with open("result.txt", encoding="utf-8") as f:
+    for line in f:
+        #print()
+        #print(line)
+        try:    
+            api_key = line.strip()
+            print(api_key)
+            client = OpenAI(api_key=api_key)
+
+
+            response = client.responses.create(
+                model="gpt-4o-mini",
+                input="ку"
+            )
+        except Exception as e:
+            print(f"Error with API key {api_key}: {e}")
+            continue
+
+print(response.usage)
